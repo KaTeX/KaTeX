c774b69de8ab4229a621ddbcc84c1156f8578d89
Ben Alpert <spicyjalapeno@gmail.com>
2013-07-14 22:46:00 -0700

    Make contains() actually work in IE8
    Summary: IE8 doesn't have indexOf on arrays!
    
    Reviewers: emily
    
    Reviewed By: emily
    
    Differential Revision: http://phabricator.khanacademy.org/D3052

--- utils.js
+++ utils.js
@@ -1,6 +1,17 @@
-function contains(list, elem) {
+function fastContains(list, elem) {
     return list.indexOf(elem) !== -1;
 }
+
+function slowContains(list, elem) {
+    for (var i = 0; i < list.length; i++) {
+        if (list[i] === elem) {
+            return true;
+        }
+    }
+    return false;
+}
+
+var contains = Array.prototype.indexOf ? fastContains : slowContains;
 
 module.exports = {
     contains: contains
