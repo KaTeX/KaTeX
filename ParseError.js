1430ebaf067f7420030651774224f28602814b88
Emily Eisenberg <emily@khanacademy.org>
2013-07-22 14:46:13 -0700

    Make ParseErrors differentiable from other Errors
    Auditors: alpert

--- ParseError.js
+++ ParseError.js
@@ -1,7 +1,10 @@
 function ParseError(message) {
-    this.message = "TeX parse error: " + message;
+    var self = new Error("TeX parse error: " + message);
+    self.name = "ParseError";
+    self.__proto__ = ParseError.prototype;
+    return self;
 }
 
-ParseError.prototype = Error.prototype;
+ParseError.prototype.__proto__ = Error.prototype;
 
 module.exports = ParseError;
