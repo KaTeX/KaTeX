6dd6032ebcf7fed1e5277ee7ed03f6e078ad05d9
Ben Alpert <spicyjalapeno@gmail.com>
2014-07-23 20:23:50 -0700

    Add permalink button to test page
    Auditors: emily

--- static/main.js
+++ static/main.js
@@ -1,6 +1,7 @@
-window.onload = function() {
+function init() {
     var input = document.getElementById("input");
     var math = document.getElementById("math");
+    var permalink = document.getElementById("permalink");
 
     if ("oninput" in input) {
         input.addEventListener("input", reprocess, false);
@@ -8,9 +9,20 @@ window.onload = function() {
         input.attachEvent("onkeyup", reprocess);
     }
 
+    permalink.addEventListener("click", function() {
+        window.location.search = "?text=" + encodeURIComponent(input.value);
+    });
+
+    var match = (/(?:^\?|&)text=([^&]+)/).exec(window.location.search);
+    if (match) {
+        input.value = decodeURIComponent(match[1]);
+    }
+
     reprocess();
 
     function reprocess() {
         katex.process(input.value, math);
     }
-};
+}
+
+init();
