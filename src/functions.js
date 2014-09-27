fa83899ae4302681a782c0448565e0f8b07beaf1
drewsberry <drew@drewsilcock.co.uk>
2014-09-27 16:47:28 +0100

    Add \LaTeX function

--- src/functions.js
+++ src/functions.js
@@ -152,6 +152,16 @@ var functions = {
         }
     },
 
+    // A LaTeX logo
+    "\\LaTeX": {
+        numArgs: 0,
+        handler: function(func) {
+            return {
+                type: "latex"
+            };
+        }
+    },
+
     "\\over": {
         numArgs: 0,
         handler: function (func) {
