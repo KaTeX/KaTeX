e57366fdea4d834d245cc8814fa040683c0ce4b7
Alexandros Sigalas <alxarch@gmail.com>
2015-03-12 20:59:53 +0200

    Adds cli option -d and --display for displayMode

--- cli.js
+++ cli.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 // Simple CLI for KaTeX.
 // Reads TeX from stdin, outputs HTML to stdout.
+//
+minimist = require("minimist");
+Settings = require("./src/Settings");
+var args = minimist(process.argv.slice(2));
+
+var settings = new Settings({
+  displayMode: args.d || args.display,
+});
 
 var katex = require("./");
 var input = "";
@@ -10,6 +18,6 @@ process.stdin.on("data", function(chunk) {
 });
 
 process.stdin.on("end", function() {
-  var output = katex.renderToString(input);
+  var output = katex.renderToString(input, settings);
   console.log(output);
 });
