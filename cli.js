42afd97f0321d4b0ae811b4f20b4323efbe30b24
Michael Bradshaw <mjbshaw@google.com>
2015-06-10 07:15:53 -0700

    Add display mode to the CLI

--- cli.js
+++ cli.js
@@ -5,11 +5,27 @@
 var katex = require("./");
 var input = "";
 
+// Skip the first two args, which are just "node" and "cli.js"
+var args = process.argv.slice(2);
+
+if (args.indexOf("--help") != -1) {
+    console.log(process.argv[0] + " " + process.argv[1] +
+                " [ --help ]" +
+                " [ --display-mode ]");
+
+    console.log("\n" +
+                "Options:");
+    console.log("  --help            Display this help message");
+    console.log("  --display-mode    Render in display mode (not inline mode)");
+    process.exit();
+}
+
 process.stdin.on("data", function(chunk) {
-  input += chunk.toString();
+    input += chunk.toString();
 });
 
 process.stdin.on("end", function() {
-  var output = katex.renderToString(input);
-  console.log(output);
+    var options = { displayMode: args.indexOf("--display-mode") != -1 };
+    var output = katex.renderToString(input, options);
+    console.log(output);
 });
