8bff74ca09bb68fa48a0be217e1f97ec3e25a6da
Martin von Gagern <gagern@ma.tum.de>
2015-07-07 00:24:40 +0200

    Change group type of array from inner to ord
    This is a consequence of Rule 8 of Appendix G of The Tex Book.

--- src/buildHTML.js
+++ src/buildHTML.js
@@ -44,7 +44,7 @@ var groupToType = {
     close: "mclose",
     inner: "minner",
     genfrac: "mord",
-    array: "minner",
+    array: "mord",
     spacing: "mord",
     punct: "mpunct",
     ordgroup: "mord",
@@ -616,7 +616,7 @@ var groupTypes = {
             }
         }
         body = makeSpan(["mtable"], cols);
-        return makeSpan(["minner"], [body], options.getColor());
+        return makeSpan(["mord"], [body], options.getColor());
     },
 
     spacing: function(group, options, prev) {
