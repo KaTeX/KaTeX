9ab1d75b9eed8615889b21d01fb96053cdf259f9
Kevin Barabash <kevinb@khanacademy.org>
2015-11-28 17:59:26 -0800

    Add support for base extended unicode characters in \text{}
    Summary:
    We'd like to support rendering extended latin characters in
    order to support rendering text from European languages within
    \text{}.
    
    This diff integrates the font files from https://github.com/Khan/MathJax-dev/pull/1
    and updates lexer.js and symbols.js so that KaTeX supports ßæœøÆŒØ within \text{}.
    The plan is to support these characters with common accents to support more
    of the latin-1 unicode block.
    
    Test Plan:
    - make test
    - run all tests (including screenshot tests) on travis-ci
    
    Reviewers: emily

--- test/katex-spec.js
+++ test/katex-spec.js
@@ -1776,3 +1776,9 @@ describe("The symbol table integraty", function() {
             .toEqual(getBuilt("\\left\\lt\\frac{1}{x}\\right\\gt"));
     });
 });
+
+describe("Unicode in \\text{}", function() {
+    it("should parse ßæœøÆŒØ", function() {
+        expect("\\text{ßæœøÆŒØ}").toParse();
+    });
+});
