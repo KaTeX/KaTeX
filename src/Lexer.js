5e8519f2b4b1f2b30f69eab06ac69b5f716a3c03
Chrismessiah <chrabd@kth.se>
2016-01-24 17:04:09 +0100

    Missed out commit??

--- src/Lexer.js
+++ src/Lexer.js
@@ -49,6 +49,7 @@ var tokenRegex = new RegExp(
     "|[!-\\[\\]-\u2027\u202A-\uD7FF\uF900-\uFFFF]" +  // single codepoint
     "|[\uD800-\uDBFF][\uDC00-\uDFFF]" +               // surrogate pair
     "|\\\\(?:[a-zA-Z]+|[^\uD800-\uDFFF])" +           // function name
+    "|[\u0391-\u03A9\u03B1-\u03C9]" +                 // Greek letters
     ")"
 );
 
