a53314931822bc2a19587e0925ff0104c1f78662
William J. Bowman <wjb@williamjbowman.com>
2017-04-21 00:30:02 -0400

    Rewrote code based on feedback

--- contrib/mathtex-script-type/mathjax-script-type.js
+++ contrib/mathtex-script-type/mathjax-script-type.js
@@ -1,28 +1,24 @@
 /* global katex */
 
-function replaceMathJaxInline(node) {
-    const katexelement = document.createElement("span");
-    katexelement.setAttribute("class", "inline-equation");
-    katex.render(node.text, katexelement);
-    node.parentNode.replaceChild(katexelement, node);
-}
-
-function replaceMathJaxDisplay(node) {
-    const katexelement = document.createElement("div");
-    katexelement.setAttribute("class", "equation");
-    katex.render(node.text, katexelement, {displayMode: true});
-    node.parentNode.replaceChild(katexelement, node);
-}
-
 {
-    const l = document.scripts.length;
-    for (let i = 0; i < l; i++) {
-        const s = document.scripts[i];
-        if (s.type === "math/tex") {
-            replaceMathJaxInline(s);
+    let scripts = document.body.getElementsByTagName("script");
+    scripts = Array.prototype.slice.call(scripts);
+    scripts.forEach(function(script) {
+        if (!script.type || !script.type.match(/math\/tex/i)) {
+            return -1;
         }
-        if (s.type === "math/tex; display") {
-            replaceMathJaxDisplay(s);
+        const display =
+              (script.type.match(/mode\s*=\s*display(;|\s|\n|$)/) != null);
+
+        const katexElement = document.createElement(display ? "div" : "span");
+        katexElement.setAttribute("class",
+                                  display ? "equation" : "inline-equation");
+        try {
+            katex.render(script.text, katexElement, {displayMode: display});
+        } catch (err) {
+            //console.error(err); linter doesn't like this
+            katexElement.textContent = script.text;
         }
-    }
+        script.parentNode.replaceChild(katexElement, script);
+    });
 }
