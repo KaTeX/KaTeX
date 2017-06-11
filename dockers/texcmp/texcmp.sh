01ea767d6a9a65d2aa42384850839b1c5d8c8643
Martin von Gagern <gagern@ma.tum.de>
2017-06-11 19:33:14 +0100

    Move npm install into creation of texcmp docker image

--- dockers/texcmp/texcmp.sh
+++ dockers/texcmp/texcmp.sh
@@ -2,7 +2,7 @@
 
 set -x
 imgname=katex/texcmp
-tag=1.2
+tag=1.3
 imgid=$(docker images | awk "/${imgname//\//\\/} *${tag//./\\.}/{print \$3}")
 cd "$(dirname "$0")" || exit $?
 if [[ -z ${imgid} ]]; then
@@ -21,9 +21,7 @@ cleanup() {
 container=$(docker create -t -u 0:0 \
     -w /KaTeX/dockers/texcmp \
     "${imgname}:${tag}" \
-    bash -c "npm install \
-&& ( cd ../../test/screenshotter && npm i js-yaml; ) \
-&& nodejs texcmp.js $*")
+    nodejs texcmp.js "$@")
 trap cleanup EXIT
 tar c dockers/texcmp/{texcmp.js,package.json} \
     test/screenshotter/{ss_data.{js,yaml},images/*-firefox.png,test.tex} \
