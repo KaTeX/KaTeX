7610b1b4633d1cae75bfc041454201c9a9cee1eb
Erik Demaine <edemaine@mit.edu>
2017-08-21 13:46:34 -0400

    Factor out replacement code

--- contrib/copy-tex/copy-tex.js
+++ contrib/copy-tex/copy-tex.js
@@ -4,17 +4,10 @@ const copyDelimiters = {
     display: ['$$', '$$'], // alternative: ['\[', '\]']
 };
 
-// Global copy handler to modify behavior on .katex elements.
-document.addEventListener('copy', function(event) {
-    const selection = window.getSelection();
-    if (selection.isCollapsed) {
-        return;  // default action OK if selection is empty
-    }
-    const fragment = selection.getRangeAt(0).cloneContents();
+// Replace .katex elements with their TeX source (<annotation> element).
+// Modifies fragment in-place.  Useful for writing your own 'copy' handler.
+window.katexReplaceWithTex = function katexReplaceWithTex(fragment) {
     const katexs = fragment.querySelectorAll('.katex');
-    if (katexs.length === 0) {
-        return;  // default action OK if no .katex elements
-    }
     // Replace .katex elements with their annotation (TeX source) descendant,
     // with inline delimiters.
     katexs.forEach(function(element) {
@@ -34,7 +27,21 @@ document.addEventListener('copy', function(event) {
                     - copyDelimiters.inline[1].length)
                 + copyDelimiters.display[1];
         });
-    event.clipboardData.setData('text/plain', fragment.textContent);
+    return fragment;
+}
+
+// Global copy handler to modify behavior on .katex elements.
+document.addEventListener('copy', function(event) {
+    const selection = window.getSelection();
+    if (selection.isCollapsed) {
+        return;  // default action OK if selection is empty
+    }
+    const fragment = selection.getRangeAt(0).cloneContents();
+    if (!fragment.querySelector('.katex')) {
+        return;  // default action OK if no .katex elements
+    }
+    event.clipboardData.setData('text/plain',
+        katexReplaceWithTex(fragment).textContent);
     // Preserve usual HTML copy/paste behavior.
     event.clipboardData.setData('text/html',
         selection.getRangeAt(0).cloneContents());
