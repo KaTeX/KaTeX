dd65085eefe6fd29c6e74372ff9ca3ae61cc67fb
Ron Kok <ronkok@comcast.net>
2017-08-29 16:05:50 -0700

    Add \smash, laps, spaces, and phantoms
    1. Support `\smash`, including the optional argument from AMS.
    
    2. Change `\llap` and `\rlap` so that they render in text style. Repeat: This *changes* KaTeX behavior.
    
    3. Add `\mathllap` and `\mathrlap`. These will act as they do  in `mathtools` and as in previous KaTeX versions of `\llap` and `\rlap`.
    
    4. Add `\mathclap` and `\clap`.
    
    5. Add `\hphantom` and \vphantom`.
    
    6. Add `\thinspace`, `\medspace`, `\thickspace`
    
    7. Add `\hspace`.
    
    This work will resolve issue #270 and parts of #50 and #164.
    
    A. Perlis has written a [concise description](https://www.math.lsu.edu/~aperlis/publications/mathclap/perlis_mathclap_24Jun2003.pdf) of items 1 thru 5. Except for `\smash`'s optional argument. It's described in the [AMS User's Guide](http://texdoc.net/texmf-dist/doc/latex/amsmath/amsldoc.pdf).
    
    Item 6 also comes from the AMS User's Guide.

--- src/functions.js
+++ src/functions.js
@@ -234,13 +234,14 @@ defineFunction("\\KaTeX", {
     };
 });
 
-defineFunction("\\phantom", {
+defineFunction(["\\phantom", "\\hphantom", "\\vphantom"], {
     numArgs: 1,
 }, function(context, args) {
     const body = args[0];
     return {
-        type: "phantom",
+        type: context.funcName.slice(1),
         value: ordargument(body),
+        body: body,
     };
 });
 
@@ -516,15 +517,42 @@ defineFunction([
     };
 });
 
-// Left and right overlap functions
-defineFunction(["\\llap", "\\rlap"], {
+// Horizontal overlap functions
+defineFunction(["\\mathllap", "\\mathrlap", "\\mathclap"], {
     numArgs: 1,
     allowedInText: true,
 }, function(context, args) {
     const body = args[0];
     return {
-        type: context.funcName.slice(1),
+        type: "lap",
+        className: context.funcName.slice(5),
         body: body,
+    };
+});
+
+// smash, with optional [bt], as in AMS
+defineFunction("\\smash", {
+    numArgs: 1,
+    numOptionalArgs: 1,
+    allowedInText: true,
+}, function(context, args) {
+    const tbArg = args[0];
+    let tb = "";
+    if (tbArg) {
+        let letter = "";
+        for (let i = 0; i < tbArg.value.length; ++i) {
+            letter = tbArg.value[i].value;
+            if (letter === "t" || letter === "b") {
+                tb += letter;
+            }
+        }
+    }
+
+    const body = args[1];
+    return {
+        type: "smash",
+        body: body,
+        tb: tb,
     };
 });
 
