03a954f53089a4fc40f658ee5cf858e9c7db3ec7
Ashish Myles <amyles@google.com>
2017-09-03 00:10:53 -0400

    To @flow: Token, Lexer, ParseError, and ParseNode.

--- src/ParseNode.js
+++ src/ParseNode.js
@@ -1,22 +1,31 @@
+// @flow
+import {LexerInterface, Token} from "./Token";
+
 /**
  * The resulting parse tree nodes of the parse tree.
  *
- * It is possible to provide position information, so that a ParseNode can
- * fulfil a role similar to a Token in error reporting.
- * For details on the corresponding properties see Token constructor.
+ * It is possible to provide position information, so that a `ParseNode` can
+ * fulfill a role similar to a `Token` in error reporting.
+ * For details on the corresponding properties see `Token` constructor.
  * Providing such information can lead to better error reporting.
- *
- * @param {string}  type       type of node, like e.g. "ordgroup"
- * @param {?object} value      type-specific representation of the node
- * @param {string}  mode       parse mode in action for this node,
- *                             "math" or "text"
- * @param {Token=} firstToken  first token of the input for this node,
- *                             will omit position information if unset
- * @param {Token=} lastToken   last token of the input for this node,
- *                             will default to firstToken if unset
  */
 export default class ParseNode {
-    constructor(type, value, mode, firstToken, lastToken) {
+    type: *;
+    value: *;
+    mode: *;
+    lexer: LexerInterface|void;
+    start: number|void;
+    end: number|void;
+
+    constructor(
+        type: string,       // type of node, like e.g. "ordgroup"
+        value: mixed,       // type-specific representation of the node
+        mode: string,       // parse mode in action for this node, "math" or "text"
+        firstToken?: Token, // first token of the input for this node,
+                            // will omit position information if unset
+        lastToken?: Token,  // last token of the input for this node,
+                            // will default to firstToken if unset
+    ) {
         this.type = type;
         this.value = value;
         this.mode = mode;
