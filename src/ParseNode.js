e745e58a32c9a54d29e2f6af97ede1b362789bba
Ashish Myles <amyles@google.com>
2017-09-09 10:39:34 -0400

    Add a `Mode` @flow type and use it in `ParseNode`.

--- src/ParseNode.js
+++ src/ParseNode.js
@@ -1,5 +1,6 @@
 // @flow
 import {LexerInterface, Token} from "./Token";
+import type {Mode} from "./types";
 
 /**
  * The resulting parse tree nodes of the parse tree.
@@ -12,7 +13,7 @@ import {LexerInterface, Token} from "./Token";
 export default class ParseNode {
     type: *;
     value: *;
-    mode: *;
+    mode: Mode;
     // TODO: We should combine these to ({lexer, start, end}|void) as they
     // should all exist together or not exist at all. That way, only a single
     // void check needs to be done to see if we have metadata.
@@ -23,7 +24,7 @@ export default class ParseNode {
     constructor(
         type: string,       // type of node, like e.g. "ordgroup"
         value: mixed,       // type-specific representation of the node
-        mode: string,       // parse mode in action for this node, "math" or "text"
+        mode: Mode,         // parse mode in action for this node, "math" or "text"
         firstToken?: Token, // first token of the input for this node,
                             // will omit position information if unset
         lastToken?: Token,  // last token of the input for this node,
---
+++ src/types.js
@@ -0,0 +1,9 @@
+// @flow
+
+/**
+ * This file consists only of basic flow types used in multiple places.
+ * For types with javascript, create separate files by themselves.
+ */
+
+export type Mode = "math" | "text";
+
