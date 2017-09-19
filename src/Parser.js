5884171843f1ee89fb1e347c6100e69ca058e513
Erik Demaine <edemaine@mit.edu>
2017-09-19 12:21:00 -0400

    Make error clearly an assertion failure

--- src/Parser.js
+++ src/Parser.js
@@ -924,7 +924,8 @@ class Parser {
             // Lexer's tokenRegex is constructed to always have matching
             // first/last characters.
             if (arg.length < 2 || arg.charAt(0) !== arg.slice(-1)) {
-                throw new ParseError("\\verb failure to parse");
+                throw new ParseError(`\\verb assertion failed -- 
+                    please report what input caused this bug`);
             }
             arg = arg.slice(1, -1);  // remove first and last char
             return new ParseFuncOrArgument(
