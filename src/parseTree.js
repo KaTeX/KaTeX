1d8cf3fa0869f14312ec95535fbeb079155d967f
Ashish Myles <marcianx@gmail.com>
2017-11-25 22:10:14 -0500

    Port katex, buildTree, parseTree to @flow. (#989)

--- src/parseTree.js
+++ src/parseTree.js
@@ -1,3 +1,4 @@
+// @flow
 /**
  * Provides a single function for parsing an expression using a Parser
  * TODO(emily): Remove this
@@ -5,10 +6,13 @@
 
 import Parser from "./Parser";
 
+import type ParseNode from "./ParseNode";
+import type Settings from "./Settings";
+
 /**
  * Parses an expression using a Parser, then returns the parsed result.
  */
-const parseTree = function(toParse, settings) {
+const parseTree = function(toParse: string, settings: Settings): ParseNode[] {
     if (!(typeof toParse === 'string' || toParse instanceof String)) {
         throw new TypeError('KaTeX can only parse string typed expression');
     }
