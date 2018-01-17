d7c2260ccbbcd8c525690d3a5a853e1f7e3ffc73
ylemkimon <ylemkimon@naver.com>
2018-01-17 17:48:06 +0900

    Add comments regarding devServer option

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,9 @@ const { targets, createConfig } = require('./webpack.common');
 const path = require('path');
 const PORT = 7936;
 
+// only the `devServer` options for the first configuration will be taken
+// into account and used for all the configurations in the array.
+//                                                dev   minify
 const katexConfig = createConfig(targets.shift(), true, false);
 katexConfig.devServer = {
     contentBase: [path.join(__dirname, 'static'), __dirname],
@@ -14,6 +17,6 @@ katexConfig.devServer = {
 };
 
 module.exports = [
-    katexConfig, //                               dev   minify
+    katexConfig,
     ...targets.map(target => createConfig(target, true, false)),
 ];
