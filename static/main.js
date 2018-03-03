cf36edaa154570fe96d8919654c01753d214f3b4
Erik Demaine <edemaine@mit.edu>
2018-03-03 12:48:12 -0500

    Add display/displayMode toggle on test page
    * Fix #1035
    * Refactor query parsing code to promote re-use
    * Also add support for (unescaped) single-character macros

--- static/main.js
+++ static/main.js
@@ -5,6 +5,14 @@
 import katex from '../katex.webpack.js';
 import './main.css';
 
+function argumentRegex(keyRegex) {
+    return new RegExp(`(?:^\\?|&)(${keyRegex})=([^&]*)`, "g");
+}
+
+function argument(keyRegex) {
+    return argumentRegex(keyRegex).exec(window.location.search);
+}
+
 function init() {
     const input = document.getElementById("input");
     let math = document.getElementById("math");
@@ -13,32 +21,38 @@ function init() {
     input.addEventListener("input", reprocess, false);
     permalink.addEventListener("click", setSearch);
 
-    let match = (/(?:^\?|&)text=([^&]*)/).exec(window.location.search);
-    if (match) {
-        input.value = decodeURIComponent(match[1]);
+    let match;
+    if ((match = argument("text"))) {
+        input.value = decodeURIComponent(match[2]);
     }
 
     const macros = {};
-    // TODO: Add toggle for displayMode.
-    // https://github.com/Khan/KaTeX/issues/1035
     const options = {displayMode: true};
-    const macroRegex = /(?:^\?|&)(?:\\|%5[Cc])([A-Za-z]+)=([^&]*)/g;
+    const macroRegex = argumentRegex("(?:\\\\|%5[Cc])[A-Za-z]+|.");
     let macroString = "";
     while ((match = macroRegex.exec(window.location.search)) !== null) {
         options.macros = macros;
-        macros["\\" + match[1]] = decodeURIComponent(match[2]);
+        macros[match[1].replace(/%5[cC]/, "\\")] = decodeURIComponent(match[2]);
         macroString += "&" + match[0].substr(1);
+    }
+
+    // Use `display=0` or `displayMode=0` to turn off displayMode.
+    if ((match = argument("display|displayMode"))) {
+        if (match[2] === "false" || match[2] === "0" || match[2] === "no") {
+            options.displayMode = false;
+            macroString += "&" + match[0].substr(1);
+        }
     }
 
     // The `before` search parameter puts normal text before the math.
     // The `after` search parameter puts normal text after the math.
     // Example use: testing baseline alignment.
-    if (/(?:^\?|&)(?:before|after)=/.test(window.location.search)) {
+    if (argument("before|after")) {
         const mathContainer = math;
         mathContainer.id = "math-container";
 
-        if ((match = /(?:^\?|&)before=([^&]*)/.exec(window.location.search))) {
-            const before = document.createTextNode(decodeURIComponent(match[1]));
+        if ((match = argument("before"))) {
+            const before = document.createTextNode(decodeURIComponent(match[2]));
             mathContainer.appendChild(before);
             macroString += "&" + match[0].substr(1);
         }
@@ -47,8 +61,8 @@ function init() {
         math.id = "math";
         mathContainer.appendChild(math);
 
-        if ((match = /(?:^\?|&)after=([^&]*)/.exec(window.location.search))) {
-            const after = document.createTextNode(decodeURIComponent(match[1]));
+        if ((match = argument("after"))) {
+            const after = document.createTextNode(decodeURIComponent(match[2]));
             mathContainer.appendChild(after);
             macroString += "&" + match[0].substr(1);
         }
