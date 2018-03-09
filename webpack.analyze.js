017eb7b91dbd48ab6870795254f8b83031b3cb93
Ryan Randall <rrandall@cainc.com>
2018-03-09 11:13:02 -0500

    Implementing analyze script (#1197)

---
+++ webpack.analyze.js
@@ -0,0 +1,13 @@
+// @flow
+const { targets, createConfig } = require('./webpack.common');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+//                                                dev   minify
+const katexConfig = createConfig(targets.shift(), false, false);
+
+katexConfig.plugins.push(new BundleAnalyzerPlugin());
+
+module.exports = [
+    katexConfig,
+];
+
