d8f136acc3d2213428d2743d223a5b54ac41fbab
Kully <adam@plot.ly>
2018-04-20 06:59:59 -0400

    wrote python scraper for mathjax functions from official doc page

--- mathjax_scraper.py
+++ mathjax_scraper.py
@@ -10,7 +10,8 @@ res_text = res.text
 start_index = re.search('<h[0-9]>Symbols', res_text).start()
 end_index = re.search('<h[0-9]>Environments', res_text).start()
 
-all_matches = re.findall("\\\\.</span>|\\\\[A-z]*</span>", res_text[start_index:end_index])
+all_matches = re.findall("\\\\.</span>|\\\\[A-z]*</span>",
+                         res_text[start_index:end_index])
 
 # remove span
 for i, m in enumerate(all_matches):
@@ -22,7 +23,8 @@ all_matches = basic_symbols + all_matches
 
 # put into dictionary
 alpha_dict = dict(zip(list(string.ascii_lowercase), [[] for i in range(26)]))
-alpha_dict['basic'] = []
+basic_label = 'basic'
+alpha_dict[basic_label] = []
 
 for m in all_matches:
     key = re.findall('[^\\\\]+', m)
@@ -30,9 +32,12 @@ for m in all_matches:
         if key[0][0].lower() in alpha_dict.keys():
             alpha_dict[key[0][0].lower()].append(m)
         else:
-            alpha_dict['basic'].append(m)
+            if key == [u'|']:
+                # | -> ASCII for clean markdown
+                key = [u'&#124;']
+            alpha_dict[basic_label].append(m)
 
-PATH = 'asdf.md'
+PATH = 'README.md'
 with open(PATH, 'r') as f:
     readme_text = f.read()
 
@@ -44,22 +49,25 @@ readme_text.find(mathjax_section_start)
 readme_text.find(mathjax_section_end)
 
 old_text = readme_text[
-    readme_text.find(mathjax_section_start) + len(mathjax_section_start):readme_text.find(mathjax_section_end)
+    readme_text.find(mathjax_section_start) +
+    len(mathjax_section_start):readme_text.find(mathjax_section_end)
 ]
 
 # construct md table
 table = ''
-main_header = '''\n|MathJax Symbols|KaTeX Support|\n|---|---|\n'''
+main_header = '''\n|MathJax Symbols| [x] KaTeX Support|\n|---|---|\n'''
 table += main_header
 
-for key in list(string.ascii_lowercase) + ['basic']:
+for key in list(string.ascii_lowercase) + [basic_label]:
     section = ''
     section_header = '| **{}** | |\n'.format(key.upper())
     section += section_header
     for name in alpha_dict[key]:
-        # TODO: checkbox goes on the right side
         section += '| {} | {} |\n'.format(name, '')
     table += section
+
+# double pipes for end of table
+table = table[:-1] + '|\n'
 
 # inject table in README.md
 readme_updated = readme_text.replace(old_text, table)
