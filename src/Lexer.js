b1689b1779a22caf1cdbcac0293691a04140152e
ylemkimon <mail@ylem.kim>
2018-07-12 18:23:18 +0900

    Separate type import statement from module import statement

--- src/Lexer.js
+++ src/Lexer.js
@@ -14,7 +14,9 @@
 
 import ParseError from "./ParseError";
 import SourceLocation from "./SourceLocation";
-import {LexerInterface, Token} from "./Token";
+import {Token} from "./Token";
+
+import type {LexerInterface} from "./Token";
 
 /* The following tokenRegex
  * - matches typical whitespace (but not NBSP etc.) using its first group
