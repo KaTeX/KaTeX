82ad7623c8e14fe76983f394379051512999e94a
ylemkimon <mail@ylem.kim>
2018-08-14 01:09:11 +0900

    Lint all JavaScript files in the root

--- update-sri.js
+++ update-sri.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const path = require("path");
 const sriToolbox = require("sri-toolbox");
 
 const version = process.argv[2];
@@ -20,12 +19,8 @@ Promise.all(process.argv.slice(3).map(file =>
     read(file, "utf8")
     .then(body => {
         // Replace size badge url
-        // 1 - url prefix: https://img.badgesize.io/Khan/KaTeX/
-        // 2 - url suffix: /dist/katex.min.js?compression=gzip 
-        const badgeRe = /(https:\/\/img\.badgesize\.io\/Khan\/KaTeX\/v)(?:.+)(\/dist\/katex\.min\.js\?compression=gzip)/g;
-        body = body.replace(badgeRe, (m, pre, post) => {
-            return pre + version + post;
-        });
+        // eslint-disable-next-line max-len
+        body = body.replace(/(https:\/\/img\.badgesize\.io\/Khan\/KaTeX\/v)(?:.+)(\/dist\/katex\.min\.js\?compression=gzip)/g, `$1${version}$2`);
 
         // Replace CDN urls
         // 1 - url prefix: "http…/KaTeX/
@@ -35,11 +30,12 @@ Promise.all(process.argv.slice(3).map(file =>
         // 5 - integrity opening quote: "
         // 6 - old hash: sha384-…
         // 7 - integrity hash algorithm: sha384
-        const cdnRe = /((["'])https?:\/\/cdn\.jsdelivr\.net\/npm\/katex@)[^\/"']+(\/([^"']+)\2(?:\s+integrity=(["'])(([^-]+)-[^"']+)\5)?)/g;
+        // eslint-disable-next-line max-len
+        const cdnRe = /((["'])https?:\/\/cdn\.jsdelivr\.net\/npm\/katex@)[^/"']+(\/([^"']+)\2(?:\s+integrity=(["'])(([^-]+)-[^"']+)\5)?)/g;
         const hashes = {};
         body = body.replace(cdnRe, (m, pre, oq1, post, file, oq2, old, algo) => {
             if (old) {
-                hashes[old] = { file, algo };
+                hashes[old] = {file, algo};
             }
             return pre + version + post;
         });
@@ -53,6 +49,7 @@ Promise.all(process.argv.slice(3).map(file =>
         )).then(() => write(file, body));
     })
 )).then(() => process.exit(0), err => {
+    // eslint-disable-next-line no-console
     console.error(err.stack);
     process.exit(1);
 });
