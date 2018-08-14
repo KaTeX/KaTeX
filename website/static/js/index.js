783a782c3214709abe891f4b4cc20f9edc00f7c4
ylemkimon <mail@ylem.kim>
2018-08-15 03:56:45 +0900

    Improve demo (REPL, sandbox) on the main page

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,19 +1,76 @@
 /* eslint-disable no-var */
 /* global katex: false */
+/* global ClipboardJS: false */
 (function() {
     var tex = document.getElementsByClassName("tex");
     Array.prototype.forEach.call(tex, function(el) {
         katex.render(el.getAttribute("data-expr"), el);
     });
 
+    var demo = document.getElementById("demo");
     var demoInput = document.getElementById("demo-input");
     var demoOutput = document.getElementById("demo-output");
+    var overlay = document.getElementById("overlay");
+    var optionsButton = document.getElementById("options");
+    var optionsPanel = document.getElementById("options-panel");
+    var maximize = document.getElementById("maximize");
+    var copyCode = document.getElementById("code");
+    var copyPermalink = document.getElementById("permalink");
+
+    var match = window.location.search.match(/[?&]data=([^&#]*)/);
+    var data = {};
+    if (match) {
+        try {
+            data = JSON.parse(decodeURIComponent(match[1]));
+        } catch (e) {
+            // eslint-disable-next-line no-console
+            console.warn(e);
+        }
+    }
+
+    if (data.code) {
+        demoInput.value = data.code;
+    }
+
+    var katexOptions = ["displayMode", "throwOnError", "errorColor", "strict",
+        "macros"].map(function(id) {
+            var el = document.getElementById(id);
+            if (el.type === "checkbox") {
+                if (typeof data[id] === "boolean") {
+                    el.checked = data[id];
+                }
+            } else if (data[id]) {
+                if (el.placeholder === "JSON") {
+                    el.value = JSON.stringify(data[id]);
+                } else {
+                    el.value = data[id];
+                }
+            }
+            return el;
+        });
+
+    function getOptions(withCode) {
+        var options = katexOptions.reduce(function(options, el) {
+            var val;
+            if (el.type === "checkbox") {
+                val = el.checked;
+            } else if (el.placeholder === "JSON") {
+                val = el.value ? JSON.parse(el.value) : undefined;
+            } else {
+                val = el.value;
+            }
+            options[el.id] = val;
+            return options;
+        }, {});
+        if (withCode) {
+            options.code = demoInput.value;
+        }
+        return options;
+    }
 
     function doDemo() {
         try {
-            katex.render(demoInput.value, demoOutput, {
-                displayMode: true,
-            });
+            katex.render(demoInput.value, demoOutput, getOptions());
         } catch (err) {
             while (demoOutput.lastChild) {
                 demoOutput.removeChild(demoOutput.lastChild);
@@ -26,8 +83,50 @@
         }
     }
 
-    demoInput.addEventListener("input", function() {
-        doDemo();
+    demoInput.addEventListener("input", doDemo);
+
+    optionsButton.addEventListener("click", function() {
+        document.body.classList.add("overlayed");
+    });
+
+    overlay.addEventListener("click", function() {
+        document.body.classList.remove("overlayed");
+        demoInput.focus();
+    });
+
+    optionsPanel.addEventListener("click", function(e) {
+        e.stopPropagation();
+    });
+
+    maximize.addEventListener("click", function() {
+        if (!demo.classList.contains("maximized")) {
+            demo.insertBefore(optionsPanel, demo.childNodes[0]);
+            demo.classList.add("maximized");
+        } else {
+            overlay.appendChild(optionsPanel);
+            demo.classList.remove("maximized");
+        }
+    });
+
+    katexOptions.map(function(el) {
+        el.addEventListener("input", doDemo);
+    });
+
+    // eslint-disable-next-line no-new
+    new ClipboardJS(copyCode, {
+        text: function() {
+            return `katex.render(${JSON.stringify(demoInput.value)
+                }, /* element */, ${JSON.stringify(getOptions())})`;
+        },
+        container: optionsPanel,
+    });
+
+    // eslint-disable-next-line no-new
+    new ClipboardJS(copyPermalink, {
+        text: function() {
+            return `${location.href}?data=${
+                encodeURIComponent(JSON.stringify(getOptions(true)))}`;
+        },
     });
 
     doDemo();
