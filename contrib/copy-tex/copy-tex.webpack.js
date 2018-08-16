efc0bb9f37568452a63db5dbbdd0b2c2de4f3580
ylemkimon <mail@ylem.kim>
2018-08-16 10:38:52 +0900

    Create separate entry point of copy-tex for webpack

---
+++ contrib/copy-tex/copy-tex.webpack.js
@@ -0,0 +1,6 @@
+/**
+ * This is the webpack entry point for KaTeX. As ECMAScript doesn't support
+ * CSS modules natively, a separate entry point is used.
+ */
+import './copy-tex.css';
+import './copy-tex.js';
