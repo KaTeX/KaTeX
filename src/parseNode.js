a3215b284e9608f81111ae34876a9b1af69772a3
ylemkimon <mail@ylem.kim>
2018-10-29 13:31:00 +0900

    Refactor `buildExpression` to depth-first traverse groups (nodes), fix spacings and \begingroup...\endgroup (#1706)
    * Refactor `buildExpression` to depth-first traverse groups (nodes)
    
    * Fix \begingroup...\endgroup
    
    * Rename traverseNodes to traverseNonSpaceNodes, update comments and tests
    
    * Update Parser.js
    
    * Update katex-spec.js.snap

--- src/parseNode.js
+++ src/parseNode.js
@@ -79,6 +79,7 @@ type ParseNodeTypes = {
         mode: Mode,
         loc?: ?SourceLocation,
         body: AnyParseNode[],
+        semisimple?: boolean,
     |},
     "raw": {|
         type: "raw",
