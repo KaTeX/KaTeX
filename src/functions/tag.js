446fd11322d9fef8a26d5b4857ca76b318a554d9
Ron Kok <ronkok@comcast.net>
2019-03-29 14:39:40 -0700

    Improve MathML for \tag

--- src/functions/tag.js
+++ src/functions/tag.js
@@ -7,17 +7,22 @@ import * as mml from "../buildMathML";
 defineFunctionBuilders({
     type: "tag",
     mathmlBuilder(group, options) {
+        const pad = new mathMLTree.MathNode("mtd", []);
+        pad.setAttribute("width", "50%");
+
         const table = new mathMLTree.MathNode("mtable", [
-            new mathMLTree.MathNode("mlabeledtr", [
-                new mathMLTree.MathNode("mtd", [
-                    mml.buildExpressionRow(group.tag, options),
-                ]),
+            new mathMLTree.MathNode("mtr", [
+                pad,
                 new mathMLTree.MathNode("mtd", [
                     mml.buildExpressionRow(group.body, options),
                 ]),
+                pad,
+                new mathMLTree.MathNode("mtd", [
+                    mml.buildExpressionRow(group.tag, options),
+                ]),
             ]),
         ]);
-        table.setAttribute("side", "right");
+        table.setAttribute("width", "100%");
         return table;
     },
 });
