80cda2fb7335e0a27b7687989ef5cc94121e769f
Ron Kok <ronkok@comcast.net>
2019-04-02 08:54:30 -0700

    Add TODO

--- src/functions/tag.js
+++ src/functions/tag.js
@@ -4,21 +4,22 @@ import mathMLTree from "../mathMLTree";
 
 import * as mml from "../buildMathML";
 
+const pad = () => {
+    const padNode = new mathMLTree.MathNode("mtd", []);
+    padNode.setAttribute("width", "50%");
+    return padNode;
+}
+
 defineFunctionBuilders({
     type: "tag",
     mathmlBuilder(group, options) {
-        const pad = new mathMLTree.MathNode("mtd", []);
-        pad.setAttribute("width", "50%");
-        const pad2 = new mathMLTree.MathNode("mtd", []);
-        pad2.setAttribute("width", "50%");
-
         const table = new mathMLTree.MathNode("mtable", [
             new mathMLTree.MathNode("mtr", [
-                pad,
+                pad(),
                 new mathMLTree.MathNode("mtd", [
                     mml.buildExpressionRow(group.body, options),
                 ]),
-                pad2,
+                pad(),
                 new mathMLTree.MathNode("mtd", [
                     mml.buildExpressionRow(group.tag, options),
                 ]),
@@ -26,6 +27,14 @@ defineFunctionBuilders({
         ]);
         table.setAttribute("width", "100%");
         return table;
+
+        // TODO: Left-aligned tags.
+        // Currently, the group and options passed here do not contain
+        // enough info to set tag alignment. `leqno` is in Settings but it is
+        // not passed to Options. On the HTML side, leqno is
+        // set by a CSS class applied in buildTree.js. That would have worked
+        // in MathML if browsers supported <mlabeledtr>. Since they don't, we
+        // need to rewrite the way this function is called.
     },
 });
 
