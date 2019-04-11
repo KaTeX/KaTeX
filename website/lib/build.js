da08d71eb8ef6ea52258fef03605552bb3d7e426
Thomas Mathew <mothas.thomas@gmail.com>
2019-04-11 13:21:48 -0700

    Use relative link to mhchem and copy-tex
    - Included relative links extensions mhchem & copy-tex in website/pages/index.html
    - Included copy commands in website/lib/build.js. Will be used in 'yarn build'
    - Included 2 more files in .gitignore

--- website/lib/build.js
+++ website/lib/build.js
@@ -16,11 +16,17 @@ fs.copySync('../dist/katex.min.js', 'static/static/katex.min.js');
 fs.copySync('../dist/katex.min.css', 'static/static/katex.min.css');
 fs.copySync('../dist/fonts', 'static/static/fonts');
 
+// copy local built KaTeX extensions
+fs.copySync('../dist/contrib/copy-tex.css', 'static/static/copy-tex.css');
+fs.copySync('../dist/contrib/copy-tex.min.js', 'static/static/copy-tex.min.js');
+fs.copySync('../dist/contrib/mhchem.min.js', 'static/static/mhchem.min.js');
+
+
 // use KaTeX from CDN on the main page for Netlify production deploy
 if (process.env.CONTEXT === 'production') {
     const version = require('../versions.json')[0];
     let indexHtml = fs.readFileSync('pages/index.html', 'utf8');
-    indexHtml = indexHtml.replace(/(["'])static\/(katex|fonts)/g,
+    indexHtml = indexHtml.replace(/(["'])static\/(katex|fonts|copy-tex|mhchem)/g,
         `$1https://cdn.jsdelivr.net/npm/katex@${version}/dist/$2`);
     fs.writeFileSync('pages/index.html', indexHtml);
 }
