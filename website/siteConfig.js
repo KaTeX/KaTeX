567fcc8eb36598801e63752a45f120312be98357
Tonton Pixel <26766930+tonton-pixel@users.noreply.github.com>
2019-06-08 10:44:53 +0200

    Update siteConfig.js
    Fixed image path

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -117,7 +117,7 @@ const users = [
     },
     {
         caption: 'Tutti Quanti Shelf',
-        image: 'https://github.com/tonton-pixel/tutti-quanti-shelf/blob/master/icons/icon.png',
+        image: 'https://raw.githubusercontent.com/tonton-pixel/tutti-quanti-shelf/master/icons/icon.png',
         infoLink: 'https://github.com/tonton-pixel/tutti-quanti-shelf/',
     },
     {
