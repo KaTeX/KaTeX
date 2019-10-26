83ef4814fc14568ff14b25960079ce9df260ec83
ylemkimon <mail@ylem.kim>
2019-10-27 03:46:11 +0900

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -168,7 +168,7 @@ const siteConfig = {
     copyright:
     'Copyright © ' +
     new Date().getFullYear() +
-    ' Khan Academy and individual contributors',
+    ' Khan Academy and other contributors',
 
     highlight: {
         // Highlight.js theme to use for syntax highlighting in code blocks
