fa8fbc0c18e5e3fe98f347ceed3a48699d561c72
ylemkimon <mail@ylem.kim>
2019-11-30 05:08:22 +0900

    Remove checkNodeType and assert/checkAtomFamily (#2137)
    We used functions with $FlowFixMe as Flow couldn't refine ParseNode 
    using its type. It seems the issue has been fixed and complicated 
    function calls can be removed.

--- src/defineFunction.js
+++ src/defineFunction.js
@@ -1,6 +1,4 @@
 // @flow
-import {checkNodeType} from "./parseNode";
-
 import type Parser from "./Parser";
 import type {ParseNode, AnyParseNode, NodeType, UnsupportedCmdParseNode}
     from "./parseNode";
@@ -228,6 +226,5 @@ export function defineFunctionBuilders<NODETYPE: NodeType>({
 // Since the corresponding buildHTML/buildMathML function expects a
 // list of elements, we normalize for different kinds of arguments
 export const ordargument = function(arg: AnyParseNode): AnyParseNode[] {
-    const node = checkNodeType(arg, "ordgroup");
-    return node ? node.body : [arg];
+    return arg.type === "ordgroup" ? arg.body : [arg];
 };
