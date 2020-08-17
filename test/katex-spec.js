b59ca675301ce849f9dba62c83dfe6ae01acea95
Ron Kok <ronkok@comcast.net>
2020-08-17 04:35:24 -0700

    feat: support Unicode ◯, U+25EF (#2430)
    * Support Unicode U+25EF \bigcirc
    
    * Update documentation

--- test/katex-spec.js
+++ test/katex-spec.js
@@ -3653,7 +3653,7 @@ describe("Unicode", function() {
     });
 
     it("should build binary operators", function() {
-        expect("±×÷∓∔∧∨∩∪≀⊎⊓⊔⊕⊖⊗⊘⊙⊚⊛⊝⊞⊟⊠⊡⊺⊻⊼⋇⋉⋊⋋⋌⋎⋏⋒⋓⩞\u22C5").toBuild(strictSettings);
+        expect("±×÷∓∔∧∨∩∪≀⊎⊓⊔⊕⊖⊗⊘⊙⊚⊛⊝◯⊞⊟⊠⊡⊺⊻⊼⋇⋉⋊⋋⋌⋎⋏⋒⋓⩞\u22C5").toBuild(strictSettings);
     });
 
     it("should build delimiters", function() {
