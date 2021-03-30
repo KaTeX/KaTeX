d89e053e4050608ce0bb5386c78f06efb5b2aefc
Young Min Kin <mail@ylem.kim>
2021-03-30 20:50:14 +0900

    chore(fonts): fix buildMetrics for Python 3

--- dockers/fonts/buildMetrics.sh
+++ dockers/fonts/buildMetrics.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Generates fontMetricsData.js
 PERL="perl"
-PYTHON=`python3`
+PYTHON="python3"
 
 cd src/metrics
 $PERL ./mapping.pl | $PYTHON ./extract_tfms.py | $PYTHON ./extract_ttfs.py | $PYTHON ./format_json.py --width > ../fontMetricsData.js
