395e27a08d3fbe81ecf2024db2b2e669f139cb3b
Erik Demaine <edemaine@mit.edu>
2021-04-21 12:28:03 -0400

    fix: Respect catcode in macro expansion and set ~'s catcode correctly
    Support single characters having a catcode of 13 (active) or 12 (other),
    and expand a macro defined for the character only if it's active.
    This enables defining `~` as a macro, but avoiding expansion in URLs.
    
    Fix #2924

--- src/Lexer.js
+++ src/Lexer.js
@@ -60,7 +60,8 @@ export default class Lexer implements LexerInterface {
     input: string;
     settings: Settings;
     tokenRegex: RegExp;
-    // category codes, only supports comment characters (14) for now
+    // Category codes. The lexer only supports comment characters (14) for now.
+    // MacroExpander additionally distinguishes active (13).
     catcodes: {[string]: number};
 
     constructor(input: string, settings: Settings) {
@@ -70,6 +71,7 @@ export default class Lexer implements LexerInterface {
         this.tokenRegex = new RegExp(tokenRegexString, 'g');
         this.catcodes = {
             "%": 14, // comment character
+            "~": 13, // active character
         };
     }
 
