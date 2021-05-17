93b56cfb0bd54bd3800e8b2aa161b3bcdd275c50
Erik Demaine <edemaine@mit.edu>
2021-05-17 18:45:11 -0400

    chore(screenshotter): Improve screenshotter for Safari (#3015)
    * Fix when Docker returns IPv4 and IPv6 addresses
    
    * Ensure images load before screenshotting
    
    * Fix bugs from review
    
    * Update test/screenshotter/test.html
    
    Co-authored-by: ylemkimon <y@ylem.kim>

--- dockers/screenshotter/screenshotter.js
+++ dockers/screenshotter/screenshotter.js
@@ -172,7 +172,8 @@ if (!seleniumURL && opts.container) {
         guessDockerIPs();
     }
     seleniumPort = cmd("docker", "port", opts.container, seleniumPort);
-    seleniumPort = seleniumPort.replace(/^.*:/, "");
+    // Docker can output two lines, such as "0.0.0.0:49156\n:::49156"
+    seleniumPort = seleniumPort.replace(/[^]*:([0-9]+)[^]*/, "$1");
 }
 if (!seleniumURL && seleniumIP) {
     seleniumURL = "http://" + seleniumIP + ":" + seleniumPort + "/wd/hub";
@@ -493,7 +494,7 @@ function takeScreenshot(key) {
     function loadFonts() {
         driver.executeAsyncScript(
                 "var callback = arguments[arguments.length - 1]; " +
-                "load_fonts(callback);")
+                "load_fonts_and_images(callback);")
             .then(waitThenScreenshot);
     }
 
