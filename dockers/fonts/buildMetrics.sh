cd5b614d478789ea9c3cab867270f48f602f3d94
ylemkimon <y@ylem.kim>
2021-08-29 06:35:02 +0900

    Add build:{fonts,metrics} script

--- dockers/fonts/buildMetrics.sh
+++ dockers/fonts/buildMetrics.sh
@@ -3,5 +3,5 @@
 PERL="perl"
 PYTHON="python3"
 
-cd $(dirname "$)")/../../src/metrics
+cd "$(dirname "$0")/../../src/metrics"
 $PERL ./mapping.pl | $PYTHON ./extract_tfms.py | $PYTHON ./extract_ttfs.py | $PYTHON ./format_json.py --width > ../fontMetricsData.js
