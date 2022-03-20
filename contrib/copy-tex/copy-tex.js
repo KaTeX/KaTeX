24aaf7f4792ae6c640c16c6cf46929d963d35bf9
Fons van der Plas <fonsvdplas@gmail.com>
2022-03-20 16:23:51 +0100

    copy-tex: Use JS to select full equation instead of CSS

--- contrib/copy-tex/copy-tex.js
+++ contrib/copy-tex/copy-tex.js
@@ -1,24 +1,41 @@
 import katexReplaceWithTex from './katex2tex';
 
-// Global copy handler to modify behavior on .katex elements.
-document.addEventListener('copy', function(event) {
+document.addEventListener('copy', function (event) {
     const selection = window.getSelection();
     if (selection.isCollapsed) {
-        return;  // default action OK if selection is empty
+        return; // default action OK if selection is empty
     }
-    const fragment = selection.getRangeAt(0).cloneContents();
-    if (!fragment.querySelector('.katex-mathml')) {
-        return;  // default action OK if no .katex-mathml elements
+    const range = selection.getRangeAt(0);
+
+    const sel_parent = range.commonAncestorContainer;
+    if (!(sel_parent instanceof Element)) {
+        return;
     }
+    const closest_katex = sel_parent.closest('.katex');
+
+    let fragment, html_contents;
+
+    if (closest_katex) {
+        // Then we are fully inside an equation. We expand the selection to be the entire equation.
+
+        fragment = /** @type Element */ (closest_katex.cloneNode(true));
+
+        if (!fragment.querySelector('.katex-mathml')) {
+            return; // default action OK if no .katex-mathml elements
+        }
+        html_contents = fragment.innerHTML;
+    } else {
+        fragment = range.cloneContents();
+        if (!fragment.querySelector('.katex-mathml')) {
+            return; // default action OK if no .katex-mathml elements
+        }
+        html_contents = Array.prototype.map.call(fragment.childNodes, (el) => (el instanceof Text ? el.textContent : el.outerHTML)).join('');
+    }
+
     // Preserve usual HTML copy/paste behavior.
-    const html = [];
-    for (let i = 0; i < fragment.childNodes.length; i++) {
-        html.push(fragment.childNodes[i].outerHTML);
-    }
-    event.clipboardData.setData('text/html', html.join(''));
+    event.clipboardData.setData('text/html', html_contents);
     // Rewrite plain-text version.
-    event.clipboardData.setData('text/plain',
-        katexReplaceWithTex(fragment).textContent);
+    event.clipboardData.setData('text/plain', katexReplaceWithTex(fragment).textContent);
     // Prevent normal copy handling.
     event.preventDefault();
 });
