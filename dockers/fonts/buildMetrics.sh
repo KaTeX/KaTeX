bccbb3941933730ef1add7d8c8d4a11ff0067edf
Ivo C.S. Wingelaar <ivo@wingelaar.be>
2022-09-17 22:47:39 +0200

    refactor: move metrics generation code
    Python's module resolution rules requires us to have a slightly
    different directory structure in order to properly share the mapping
    code in a separate file.

--- dockers/fonts/buildMetrics.sh
+++ dockers/fonts/buildMetrics.sh
@@ -3,5 +3,5 @@
 PERL="perl"
 PYTHON="python3"
 
-cd "$(dirname "$0")/../../src/metrics"
-$PERL ./mapping.pl | $PYTHON ./extract_tfms.py | $PYTHON ./extract_ttfs.py | $PYTHON ./format_json.py --width > ../fontMetricsData.js
+cd "$(dirname "$0")/../../src/fonts/metrics"
+$PERL ./mapping.pl | $PYTHON ./extract_tfms.py | $PYTHON ./extract_ttfs.py | $PYTHON ./format_json.py --width > ../../fontMetricsData.js
