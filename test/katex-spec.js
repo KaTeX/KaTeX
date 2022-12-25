c7db6148eaeb8995511da3a0a3092cb914556e95
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-12-25 14:13:11 -0500

    spelling: compatibility
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- test/katex-spec.js
+++ test/katex-spec.js
@@ -1411,7 +1411,7 @@ describe("A TeX-compliant parser", function() {
             r`\frac x \mathllap y`,
             // This actually doesn't work in real TeX, but it is suprisingly
             // hard to get this to correctly work. So, we take hit of very small
-            // amounts of non-compatiblity in order for the rest of the tests to
+            // amounts of non-compatibility in order for the rest of the tests to
             // work
             // r`\llap \frac x y`,
             r`\mathllap \mathllap x`,
