3c4d289ee8ee25cd1839ab00b1a360a0f94b11d1
Ron Kok <ronkok55@outlook.com>
2023-01-18 13:42:56 -0800

    Evade wideCharacterFont() when inapplicable

--- src/buildCommon.js
+++ src/buildCommon.js
@@ -165,9 +165,11 @@ const makeOrd = function<NODETYPE: "spacing" | "mathord" | "textord">(
     // Math mode or Old font (i.e. \rm)
     const isFont = mode === "math" || (mode === "text" && options.font);
     const fontOrFamily = isFont ? options.font : options.fontFamily;
-    if (text.charCodeAt(0) === 0xD835) {
+    const [wideFontName, wideFontClass] = text.charCodeAt(0) !== 0xD835
+      ? ["", ""]
+      : wideCharacterFont(text, mode);
+    if (wideFontName.length > 0) {
         // surrogate pairs get special treatment
-        const [wideFontName, wideFontClass] = wideCharacterFont(text, mode);
         return makeSymbol(text, wideFontName, mode, options,
             classes.concat(wideFontClass));
     } else if (fontOrFamily) {
