80243573aded93a2bf375c796f754575209ea4f7
Grigoriy Reshetniak <grigoriy.reshetniak@gmail.com>
2026-07-06 23:11:15 +0300

    increase wait for webkit on CI

--- dockers/screenshotter/screenshotter.js
+++ dockers/screenshotter/screenshotter.js
@@ -72,13 +72,6 @@ const opts = program
     .parse(process.argv)
     .opts();
 
-
-// WebKit (safari) needs extra settle time for glyph rasterization to be
-// deterministic; default to 0.5s there unless the user overrode --wait.
-if (opts.wait === undefined) {
-    opts.wait = (opts.browser === "safari" || opts.browser === "webkit") ? 0.5 : 0;
-}
-
 let listOfCases;
 if (opts.include) {
     listOfCases = opts.include.split(",");
